6ad508577eed38e96edfe29db7cef8983358f40a
DELL-1 <DELL-1@DESKTOP-GAMS896>
2023-11-28 17:19:10 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica__7_conversiones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double v1, farh1;
+            v1 = double.Parse(textBox1.Text);
+            farh1 = v1 * (9.0 / 5.0) + 32;
+            textBox2.Text = farh1.ToString();
+            label2.Text = "FHARENHEIT";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double v1, farh1;
+            v1 = double.Parse(textBox1.Text);
+            farh1 = v1 * (9.0 / 5.0) + 32;
+            textBox2.Text = farh1.ToString();
+            label2.Text = "CENTIGRADOS";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+        }
+    }
+}
